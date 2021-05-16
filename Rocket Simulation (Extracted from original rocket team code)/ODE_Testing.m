b0ec0cd3e79aab521fec8395699cc197acbd048c
Wiler Sanchez <Wilersanchez@ufl.edu>
2021-05-16 02:08:43 -0400

    Add files via upload

---
+++ Rocket Simulation (Extracted from original rocket team code)/ODE_Testing.m
@@ -0,0 +1,88 @@
+% This script is to compare ODE solvers using a simplified physics equation
+% that omits certain parameters
+clc; clear;
+
+%
+% wind components (m/s)
+%
+global windh;
+global windv;
+windh = 10;
+windv = 0;
+
+[t0,y0] = ode45(@rocket_ode,[0 10],[0 0 0 0 pi/2 0]);
+y0(:,1:4) = y0(:,1:4)/.3048;
+y0(:,5:6) = y0(:,5:6)*180/pi;
+[apogee0,index0] = max(-y0(:,2));
+disp(['ode45   Apogee ',num2str(apogee0),' (ft)']);
+opts = odeset('stats','on','reltol',1.e-4);
+[~,~] = ode113(@rocket_ode,[0 10],[0 0 0 0 pi/2 0],opts);
+
+[t1,y1] = ode23(@rocket_ode,[0 10],[0 0 0 0 pi/2 0]);
+y1(:,1:4) = y1(:,1:4)/.3048;
+y1(:,5:6) = y1(:,5:6)*180/pi;
+[apogee1,index1] = max(-y1(:,2));
+disp(['ode23   Apogee ',num2str(apogee1),' (ft)']);
+opts = odeset('stats','on','reltol',1.e-4);
+[~,~] = ode113(@rocket_ode,[0 10],[0 0 0 0 pi/2 0],opts);
+
+[t2,y2] = ode113(@rocket_ode,[0 10],[0 0 0 0 pi/2 0]);
+y2(:,1:4) = y2(:,1:4)/.3048;
+y2(:,5:6) = y2(:,5:6)*180/pi;
+[apogee2,index2] = max(-y2(:,2));
+disp(['ode113  Apogee ',num2str(apogee2),' (ft)']);
+opts = odeset('stats','on','reltol',1.e-4);
+[~,~] = ode113(@rocket_ode,[0 10],[0 0 0 0 pi/2 0],opts);
+
+[t3,y3] = ode15s(@rocket_ode,[0 10],[0 0 0 0 pi/2 0]);
+y3(:,1:4) = y3(:,1:4)/.3048;
+y3(:,5:6) = y3(:,5:6)*180/pi;
+[apogee3,index3] = max(-y3(:,2));
+disp(['ode15s  Apogee ',num2str(apogee3),' (ft)']);
+opts = odeset('stats','on','reltol',1.e-4);
+[~,~] = ode113(@rocket_ode,[0 10],[0 0 0 0 pi/2 0],opts);
+
+[t4,y4] = ode23s(@rocket_ode,[0 10],[0 0 0 0 pi/2 0]);
+y4(:,1:4) = y4(:,1:4)/.3048;
+y4(:,5:6) = y4(:,5:6)*180/pi;
+[apogee4,index4] = max(-y4(:,2));
+disp(['ode23s  Apogee ',num2str(apogee4),' (ft)']);
+opts = odeset('stats','on','reltol',1.e-4);
+[~,~] = ode113(@rocket_ode,[0 10],[0 0 0 0 pi/2 0],opts);
+
+[t5,y5] = ode23t(@rocket_ode,[0 10],[0 0 0 0 pi/2 0]);
+y5(:,1:4) = y5(:,1:4)/.3048;
+y5(:,5:6) = y5(:,5:6)*180/pi;
+[apogee5,index5] = max(-y5(:,2));
+disp(['ode23t  Apogee ',num2str(apogee5),' (ft)']);
+opts = odeset('stats','on','reltol',1.e-4);
+[~,~] = ode113(@rocket_ode,[0 10],[0 0 0 0 pi/2 0],opts);
+
+[t6,y6] = ode23tb(@rocket_ode,[0 10],[0 0 0 0 pi/2 0]);
+y6(:,1:4) = y6(:,1:4)/.3048;
+y6(:,5:6) = y6(:,5:6)*180/pi;
+[apogee6,index6] = max(-y6(:,2));
+disp(['ode23tb Apogee ',num2str(apogee6),' (ft)']);
+opts = odeset('stats','on','reltol',1.e-4);
+[~,~] = ode113(@rocket_ode,[0 10],[0 0 0 0 pi/2 0],opts);
+%
+% Plot trajectory
+%
+figure(1)
+plot(y0(:,1),-y0(:,2),'k',y1(:,1),-y1(:,2),'r',y2(:,1),-y2(:,2),'b' ...
+    ,y3(:,1),-y3(:,2),'g',y4(:,1),-y4(:,2),'p' ...
+    ,y5(:,1),-y5(:,2),'o',y6(:,1),-y6(:,2),'y');
+xlabel('Downrange (ft)');
+ylabel('Altitude (ft)');
+legend('ODE45','ODE23','ODE113','ODE15s','ODE23s','ODE23t','ODE23tb');
+
+%
+% Plot velocity
+%
+figure(2)
+plot(t0,y0(:,3),'k',t1,y1(:,3),'r',t2,y2(:,3),'b' ...
+    ,y3(:,1),-y3(:,2),'g',y4(:,1),-y4(:,2),'p' ...
+    ,y5(:,1),-y5(:,2),'o',y6(:,1),-y6(:,2),'y');
+xlabel('Time (s)');
+ylabel('Forward Velocity (m/s)');
+legend('ODE45','ODE23','ODE113','ODE15s','ODE23s','ODE23t','ODE23tb');
---
+++ Rocket Simulation (Extracted from original rocket team code)/rocket_ode.m
@@ -0,0 +1,116 @@
+function ydot = rocket_ode(t,y)
+% y = [downrange distance, -altitude, forward velocity, transverse velocity,
+%      pitch angle, pitch rate, side range, side velocity, roll angle, roll
+%      rate, yaw angle, yaw rate]
+
+
+%
+% wind components (m/s)
+%
+global windh;
+global windv;
+
+
+%
+% define constants
+%
+g = 9.80665;                % gravitational acceleration (m/s^2) (assume constant for all altitudes)
+rho = var_density(-y(2));   % density of air [kg/m^3]
+xcp = 2.66;                 % distance from nose to center of pressure (m)
+xcg = 2.32;                 % distance from nose to center of gravity (m)
+I = 23.499;                 % moment of inertia (kg*m^2) (assume constant)
+dfus = 0.14;                % diameter of fuselage (m) 
+mMinusPropellant = 14.659;  % mass of rocket without propellant (kg)
+
+%
+% define fin planform dimensions (m)
+%
+fin_span  = 0.127;  % fin span (root to tip)
+fin_chord = 0.1905; % mean fin chord length
+
+%
+% compute reference area for fuselage (m^2)
+%
+A = pi*(dfus/2)^2;
+
+%
+% compute planform area for fin (m^2)
+%
+S = fin_span * fin_chord;
+
+%
+% compute body-fixed axis velocities (m/s)
+%
+u = y(3) + windh*cos(y(5)) + windv*sin(y(5)); %forward velocity
+w = y(4) + windh*sin(y(5)) + windv*cos(y(5)); %transverse velocity
+
+%
+% compute total velocity (m/s)
+%
+V = sqrt(u.^2 + w.^2);
+
+%
+% compute angle of attack (radians)
+% (set angle of attack to pi/2 when forward velocity is 0 to avoid divide-by-zero error);
+%
+if u ~= 0
+    alpha = atan(w/u);
+else
+    alpha = pi/2;
+end
+
+%
+% define coefficients of lift and drag (as function of angle of attack in degrees)
+%
+CLofus = 0;                     % CLo for fuselage
+CLafus = 0;                     % CLalpha for fuselage
+CDofus = 0.7;                   % CDo for fuselage
+CDafus = 0.1;                   % CDalpha for fuselage
+
+[CLfin, CDfin] = rocket_fin_coefficients(alpha);    % CL and CD for fins
+
+%
+% compute lift and drag at this velocity and angle-of-attack
+%
+
+%
+% compute lift using fin and fuselage aerodynamics
+%
+L = .5*rho*V^2*(A*(CLofus + CLafus*(alpha*180/pi))  ...
+           + S*CLfin);
+
+%
+% compute drag using fin and fuselage aerodynamics
+%
+D = .5*rho*V^2*(A*(CDofus + CDafus*(alpha*180/pi)) ...
+           + S*CDfin);
+
+%
+% generate the thrust and mass of propellant
+%
+T  = rocket_thrust(t);
+mp = rocket_mass(t);
+
+
+%
+% generate weight
+%
+m = mp + mMinusPropellant;
+W = m*g;
+
+%
+% end simulation if rocket has hit ground already
+%
+if y(2) > 1
+    y = y*0;
+end
+
+%
+% generate state derivatives
+%
+ydot(1,1) = y(3)*cos(y(5)) + y(4)*sin(y(5)); % Downrange
+ydot(2,1) = -y(3)*sin(y(5)) + y(4)*cos(y(5)); 
+ydot(3,1) = (-W*sin(y(5)) + T + L*sin(alpha) - D*cos(alpha))/m;
+ydot(4,1) = (W*cos(y(5)) - L*cos(alpha) - D*sin(alpha))/m;
+ydot(5,1) = y(6);
+ydot(6,1) = -(xcp-xcg)*(L*cos(alpha) + D*sin(alpha))/I;
