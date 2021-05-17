b35f23d912090d2016dbb054d7c79662fd4acbec
Wiler Sanchez <Wilersanchez@ufl.edu>
2021-05-17 12:59:01 -0400

    Changes to 'Var_Density'
    I added temperature and pressure as an output for the function. I renamed the function to 'var_stdatm' to better represent the function.

--- Rocket Simulation (Extracted from original rocket team code)/rocket_ode.m
+++ Rocket Simulation (Extracted from original rocket team code)/rocket_ode.m
@@ -15,13 +15,13 @@ global windv;
 %
 % define constants
 %
-g = 9.80665;                % gravitational acceleration (m/s^2) (assume constant for all altitudes)
-rho = var_density(-y(2));   % density of air [kg/m^3]
-xcp = 2.66;                 % distance from nose to center of pressure (m)
-xcg = 2.32;                 % distance from nose to center of gravity (m)
-I = 23.499;                 % moment of inertia (kg*m^2) (assume constant)
-dfus = 0.14;                % diameter of fuselage (m) 
-mMinusPropellant = 14.659;  % mass of rocket without propellant (kg)
+g = 9.80665;                    % gravitational acceleration (m/s^2) (assume constant for all altitudes)
+[rho,T,P] = var_stdatm(-y(2));  % Variable density, temperature, and pressure of air
+xcp = 2.66;                     % distance from nose to center of pressure (m)
+xcg = 2.32;                     % distance from nose to center of gravity (m)
+I = 23.499;                     % moment of inertia (kg*m^2) (assume constant)
+dfus = 0.14;                    % diameter of fuselage (m) 
+mMinusPropellant = 14.659;      % mass of rocket without propellant (kg)
 
 %
 % define fin planform dimensions (m)
